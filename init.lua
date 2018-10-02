3edcc3299dc839e56530ead96ad1ead5b51f880e
BuildTools <unconfigured@null.spigotmc.org>
2018-10-02 20:47:26 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+  --[[lit-meta
+    name = "threst"
+    version = "0.0.1"
+    dependencies = {}
+    description = "Threaded REST webservice lua library"
+    tags = { "lua", "lit", "luvit" }
+    license = "MIT"
+    author = { name = "ExtraVeral", email = "tbmale@yahoo.com" }
+    homepage = "https://github.com/tbmale/threst"
+  ]]
+  
